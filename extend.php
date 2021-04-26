6bd61671d423d4b7eefe89d7b17d2e5dd26b17d4
imshuzai <75674647+imshuzai@users.noreply.github.com>
2021-04-27 00:54:31 +0800

    Update extend.php

--- extend.php
+++ extend.php
@@ -13,13 +13,11 @@ return [
         ->configure(function (Configurator $config) {
             $config->BBCodes->addCustom(
                 '[metingjs server="{SERVER}" id="{ID}" type="{TYPE}" list-folded="{LISTFOLDED}"]',
-                '<span class="aplayer" server="{SERVER}" type="{TYPE}" id="{ID}" list-folded="{LISTFOLDED}">
-                 </span>'
+                '<span class="aplayer" server="{SERVER}" type="{TYPE}" id="{ID}" list-folded="{LISTFOLDED}"></span>'
             );
 	    $config->BBCodes->addCustom(
                 '[aplayer name="{NAME}" artist="{ARTIST}" url="{URL}" cover="{COVER}" lrc="{LRC}"]',
-                '<span class="aplayer" name="{NAME}" artist="{ARTIST}" url="{URL}" cover="{COVER}" lrc="{LRC}">
-                 </span>'
+                '<span class="aplayer" name="{NAME}" artist="{ARTIST}" url="{URL}" cover="{COVER}" lrc="{LRC}"></span>'
             );
         })
 ];
