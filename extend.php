961ea1a9a80730b7fd9f97b25553ff07a9ce0a16
imshuzai <75674647+imshuzai@users.noreply.github.com>
2021-04-27 01:51:02 +0800

    Update extend.php

--- extend.php
+++ extend.php
@@ -6,18 +6,18 @@ use s9e\TextFormatter\Configurator;
 
 return [
     (new Extend\Frontend('forum'))
-        ->content(function (Document $document) {
-            $document->head[] = '<link rel="stylesheet" type="text/css" href="../assets/extensions/imshuzai-aplayer-metingjs-for-flarum/APlayer.min.css"><script src="../assets/extensions/imshuzai-aplayer-metingjs-for-flarum/APlayer.min.js"></script><script src="../assets/extensions/imshuzai-aplayer-metingjs-for-flarum/Meting.min.js"></script>';
-        }), 
+        ->js(__DIR__.'/assets/Meting.min.js')
+	->js(__DIR__.'/assets/APlayer.min.js')
+        ->css(__DIR__.'/assets/APlayer.min.css'),
     (new Extend\Formatter)
         ->configure(function (Configurator $config) {
             $config->BBCodes->addCustom(
                 '[metingjs server="{SERVER}" id="{ID}" type="{TYPE}" list-folded="{LISTFOLDED}"]',
-                '<span class="aplayer" server="{SERVER}" type="{TYPE}" id="{ID}" list-folded="{LISTFOLDED}"></span>'
+                '<div><meting-js server="{SERVER}" type="{TYPE}" id="{ID}" list-folded="{LISTFOLDED}"></meting-js></div>'
             );
 	    $config->BBCodes->addCustom(
                 '[aplayer name="{NAME}" artist="{ARTIST}" url="{URL}" cover="{COVER}" lrc="{LRC}"]',
-                '<span class="aplayer" name="{NAME}" artist="{ARTIST}" url="{URL}" cover="{COVER}" lrc="{LRC}"></span>'
+                '<div><meting-js name="{NAME}" artist="{ARTIST}" url="{URL}" cover="{COVER}" lrc="{LRC}"></meting-js></div>'
             );
         })
 ];
