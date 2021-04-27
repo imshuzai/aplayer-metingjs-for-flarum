8df9b0c6d5d80ccfe7025b0375b9362eb6643941
imshuzai <75674647+imshuzai@users.noreply.github.com>
2021-04-27 14:24:00 +0800

    Update extend.php

--- extend.php
+++ extend.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace Shuzai\Aplayer;
-
 use Flarum\Extend;
 use Flarum\Frontend\Document;
 use s9e\TextFormatter\Configurator;
@@ -14,12 +12,12 @@ return [
     (new Extend\Formatter)
         ->configure(function (Configurator $config) {
             $config->BBCodes->addCustom(
-                '[metingjs server="{SERVER?}" id="{ID?}" type="{TYPE?}" list-folded="{LISTFOLDED?}"]',
-                '<div><meting-js server="{SERVER}" type="{TYPE}" id="{ID}" list-folded="{LISTFOLDED}"></meting-js></div>'
+                '[metingjs server="{TEXT1}" id="{TEXT2}" type="{TEXT3}" list-folded="{TEXT4}"]',
+                '<div><meting-js server="{TEXT1}" type="{TEXT3}" id="{TEXT2}" list-folded="{TEXT4}"></meting-js></div>'
             );
 	    $config->BBCodes->addCustom(
-                '[aplayer name="{NAME?}" artist="{ARTIST?}" url="{URL?}" cover="{COVER?}" lrc="{LRC?}"]',
-                '<div><meting-js name="{NAME}" artist="{ARTIST}" url="{URL}" cover="{COVER}" lrc="{LRC}"></meting-js></div>'
+                '[aplayer name="{TEXT1}" artist="{TEXT2}" url="{TEXT3}" cover="{TEXT4}" lrc="{TEXT5}"]',
+                '<div><meting-js name="{TEXT1}" artist="{TEXT2}" url="{TEXT3}" cover="{TEXT4}" lrc="{TEXT5}"></meting-js></div>'
             );
         })
 ];
