83fc1df6d7187b13f9eaf4abf4076872028454b1
imshuzai <75674647+imshuzai@users.noreply.github.com>
2021-05-29 22:48:07 +0800

    Update extend.php

--- extend.php
+++ extend.php
@@ -9,6 +9,7 @@ return [
         ->content(function (Document $document) {
             $document->head[] = '
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/aplayer/dist/APlayer.min.css">
+	<script async src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>
 	<script async src="https://cdn.jsdelivr.net/npm/aplayer/dist/APlayer.min.js"></script>
 	<script async src="https://cdn.jsdelivr.net/npm/meting@2.0.1/dist/Meting.min.js"></script>';
         }),
@@ -21,6 +22,41 @@ return [
 	    $config->BBCodes->addCustom(
                 '[aplayer name="{TEXT1}" artist="{TEXT2}" url="{TEXT3}" cover="{TEXT4?}" lrc="{TEXT5?}"]',
                 '<div><meting-js name="{TEXT1}" artist="{TEXT2}" url="{TEXT3}" cover="{TEXT4}" lrc="{TEXT5}"></meting-js></div>'
+            );
+	    $config->BBCodes->addCustom(
+                '[aplayer-hls name="{TEXT1}" artist="{TEXT2}" url="{TEXT3}" cover="{TEXT4?}"]',
+                '<script>
+		 const ap = new APlayer({
+    		 container: document.getElementById('aplayer'),
+    		 audio: [{
+        	 name: '{TEXT1}',
+        	 artist: '{TEXT2}',
+        	 url: '{TEXT3}',
+        	 cover: '{TEXT4}',
+        	 type: 'hls'
+    		 }]
+		 });
+		 </script>'
+            );
+            $config->BBCodes->addCustom(
+                '[song-in-list name="{TEXT1}" artist="{TEXT2}" url="{TEXT3}" cover="{TEXT4?}" lrc="{TEXT5?}"]',
+                '{
+            	  name: '{TEXT1}',
+            	  artist: '{TEXT2}',
+            	  url: '{TEXT3}',
+            	  cover: '{TEXT4}',
+            	  lrc: '{TEXT5}',
+        	  }'
+            );
+	    $config->BBCodes->addCustom(
+                '[aplayer-list]{TEXT6}[/aplayer-list]',
+                '<script>
+		 const ap = new APlayer({
+    		 container: document.getElementById('player'),
+    	         lrcType: 1,
+    		 audio: [{TEXT6}]
+		 });
+		 </script>'
             );
         })
 ];
