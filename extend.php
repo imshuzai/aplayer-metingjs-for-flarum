457fa67779a15ad015d1d266d66bce091b0ff0e2
imshuzai <75674647+imshuzai@users.noreply.github.com>
2021-06-15 17:22:37 +0800

    Update extend.php

--- extend.php
+++ extend.php
@@ -9,9 +9,14 @@ return [
         ->content(function (Document $document) {
             $document->head[] = '
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/aplayer/dist/APlayer.min.css">
-	<!-- <script async src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script> -->
+	<script async src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>
+	<script src="https://cdn.jsdelivr.net/npm/colorthief@2.0.2/src/color-thief.js"></script>
 	<script async src="https://cdn.jsdelivr.net/npm/aplayer/dist/APlayer.min.js"></script>
-	<script async src="https://cdn.jsdelivr.net/npm/meting@2.0.1/dist/Meting.min.js"></script>';
+	<script async src="https://cdn.jsdelivr.net/npm/meting@2.0.1/dist/Meting.min.js"></script>
+	<script src="//cdn.jsdelivr.net/npm/vue-aplayer"></script>
+	<script>
+  	Vue.component('aplayer', VueAPlayer)
+	</script>';
         }),
     (new Extend\Formatter)
         ->configure(function (Configurator $config) {
@@ -21,7 +26,15 @@ return [
             );
 	    $config->BBCodes->addCustom(
                 '[aplayer name="{TEXT1}" artist="{TEXT2}" url="{TEXT3}" cover="{TEXT4?}" lrc="{TEXT5?}"]',
-                '<div><meting-js name="{TEXT1}" artist="{TEXT2}" url="{TEXT3}" cover="{TEXT4}" lrc="{TEXT5}"></meting-js></div>'
+                '<div><aplayer 
+  			:music="{
+    				title: '{TEXT1}',
+    				artist: '{TEXT2}',
+    				src: '{TEXT3}',
+    				pic: '{TEXT4}',
+				lrc: '{TEXT5}'
+  				}"
+				/></div>'
             );
         })
 ];
