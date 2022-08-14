61c7adb729506f3b104f7cd552455fa889ab4f15
imshuzai <75674647+imshuzai@users.noreply.github.com>
2022-08-15 00:24:02 +0800

    Update extend.php

--- extend.php
+++ extend.php
@@ -8,8 +8,8 @@ return [
     (new Extend\Frontend('forum'))
         ->content(function (Document $document) {
             $document->head[] = '
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aplayer/1.10.1/APlayer.min.css">
-	<script async src="https://cdnjs.cloudflare.com/ajax/libs/aplayer/1.10.1/APlayer.min.js"></script>
+	<link rel="stylesheet" href="https://unpkg.com/browse/aplayer@1.10.1/dist/APlayer.min.css">
+	<script async src="https://unpkg.com/browse/aplayer@1.10.1/dist/APlayer.min.js"></script>
 	<script async src="https://unpkg.com/meting@2.0.1/dist/Meting.min.js"></script>';
         }),
     (new Extend\Formatter)
